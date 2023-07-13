7afcc5c8549e4650b115aa4b309f7df615e3f3f8
akashirani110 <hirani.akash110@gmail.com>
2023-07-13 21:08:32 +1000

    Set status and winner - task-5

--- sports/db/queries.go
+++ sports/db/queries.go
@@ -12,7 +12,17 @@ func getEventsQueries() map[string]string {
 				name,
 				advertised_start_time,
 				location,
-				description
+				description,
+				CASE
+        			WHEN (datetime('now', 'localtime') < advertised_start_time) THEN 'UPCOMING'
+        			WHEN (datetime('now', 'localtime') >= advertised_start_time) THEN
+            			CASE
+                			WHEN (datetime('now', 'localtime') > advertised_start_time) THEN 'COMPLETED'
+                			ELSE 'IN_PROGRESS'
+            			END
+        			ELSE 'STATUS_UNSPECIFIED'
+    			END AS status,
+			    winner
 			FROM sports_events
 		`,
 	}
--- sports/db/sports.go
+++ sports/db/sports.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"sports/proto/sports"
 	"sync"
+	"syreclabs.com/go/faker"
 	"time"
 )
 
@@ -42,7 +43,7 @@ func (s *sportsRepo) List(filter *sports.ListEventsFilter) ([]*sports.SportsEven
 		query string
 		args  []interface{}
 	)
-
+	// select sportsList query lists all the sports events
 	query = getEventsQueries()[sportsList]
 
 	rows, err := s.db.Query(query, args...)
@@ -60,8 +61,10 @@ func (s *sportsRepo) scanEvents(rows *sql.Rows) ([]*sports.SportsEvent, error) {
 	for rows.Next() {
 		var event sports.SportsEvent
 		var advertisedStart time.Time
+		var statusString string
+		var winner sql.NullString
 
-		if err := rows.Scan(&event.Id, &event.Name, &advertisedStart, &event.Location, &event.Description); err != nil {
+		if err := rows.Scan(&event.Id, &event.Name, &advertisedStart, &event.Location, &event.Description, &statusString, &winner); err != nil {
 			if err == sql.ErrNoRows {
 				return nil, nil
 			}
@@ -70,14 +73,43 @@ func (s *sportsRepo) scanEvents(rows *sql.Rows) ([]*sports.SportsEvent, error) {
 		}
 
 		ts, err := ptypes.TimestampProto(advertisedStart)
+		// convert the status string to the corresponding enum value
+		statusValue := sports.EventStatus_value[statusString]
+
+		statusEnum := sports.EventStatus(statusValue)
+
 		if err != nil {
 			return nil, err
 		}
 
 		event.AdvertisedStartTime = ts
+		// update the field
+		event.Status = &statusEnum
+		// check if the winner value is valid (not null)
+		if winner.Valid {
+			event.Winner = &winner.String
+		} else {
+			// check if the status is COMPLETED
+			if statusEnum == sports.EventStatus_COMPLETED {
+				// set the winner of the completed sports event
+				winner.String, err = generateRandomWinner()
+				if err != nil {
+					return nil, err
+				}
+				event.Winner = &winner.String
+			} else {
+				// else set the winner as nil
+				event.Winner = nil
+			}
+		}
 
 		events = append(events, &event)
 	}
 
 	return events, nil
+}
+
+// generates random team names for the winner
+func generateRandomWinner() (string, error) {
+	return faker.Team().Name(), nil
 }
